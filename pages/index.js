85ca8ba75672ba3c735c997fbcc9798394a9de43
Davi Garcia <davigs@hotmail.com>
2024-03-03 19:46:57 -0300

    update initial message

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1>Sic Parvis Magna</h1>
+      <h1>Carol, te amo! Confia, esse é só o começo! Vai dar tudo certo!</h1>
+      <h1>Confia, esse é só o começo!</h1>
+      <h1>Vai dar tudo certo!</h1>
     </div>
   );
 };

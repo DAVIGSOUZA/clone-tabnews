cb2d436619030bb59ffdca6ad3ad34d465ae5d23
Davi Garcia <davigs@hotmail.com>
2024-03-03 19:48:09 -0300

    fix message

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Carol, te amo! Confia, esse é só o começo! Vai dar tudo certo!</h1>
+      <h1>Carol, te amo!</h1>
       <h1>Confia, esse é só o começo!</h1>
       <h1>Vai dar tudo certo!</h1>
     </div>

867702f6b5761687792e2a518cbd021e26c723e4
Davi Garcia <davigs@hotmail.com>
2024-03-12 23:13:38 -0300

    improve `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,39 @@
 import database from "infra/database.js";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "Tudo ok!" });
+  const updateAt = new Date().toISOString();
+
+  const databaseQueryVersion = await database.query("SHOW server_version;");
+
+  const databaseVersion = databaseQueryVersion.rows[0].server_version;
+
+  const databaseQueryMaxConnections = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnections =
+    databaseQueryMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseQueryOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnections =
+    databaseQueryOpenedConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 };
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const db = responseBody.dependencies.database;
+
+  expect(db.version).toEqual("16.0");
+  expect(db.max_connections).toEqual(100);
+  expect(db.opened_connections).toEqual(1);
 });

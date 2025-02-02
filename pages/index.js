75fd232edbf1b0a6b63573652aca1c463bbf6720
Davi Garcia <davigs@hotmail.com>
2025-02-02 15:02:36 -0300

    update content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Carol, te amo!</h1>
-      <h1>Confia, esse é só o começo!</h1>
-      <h1>Vai dar tudo certo!</h1>
+      <h1>Bora pra cima!</h1>
     </div>
   );
 };
